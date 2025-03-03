1bf4df3810ad970e1da7ff9d66352e2fbd440f04
SaritaKumari24 <saritaakumari24@gmail.com>
2025-03-03 12:39:09 +0530

    admin dashboard

---
+++ resources/views/components/layouts/adminLayout.blade.php
@@ -0,0 +1,81 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Datum | CRM Admin Dashboard Template</title>
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico')}}">
+      <!-- Library / Plugin Css Build -->
+      <link rel="stylesheet" href="{{asset('assets/css/core/libs.min.css')}}">
+      <link rel="stylesheet" href="{{asset('assets/css/datum.min.css?v=1.0.0')}}">
+      <!-- Custom Css -->
+      <link rel="stylesheet" href="{{asset('assets/css/custom.min.css?v=1.0.0')}}">
+      <!-- Customizer Css -->
+      <link rel="stylesheet" href="{{asset('assets/css/customizer.min.css?v=1.0.0')}}">
+      <!-- font-awesome css -->
+      <link rel="stylesheet" href="{{asset('assets/vendor/font-awesome/css/all.min.css?v=1.0.0')}}">
+      
+      <link href="../../css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+      <link href="../../css2-1?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">  </head>
+  <body class="  ">
+        {{ $slot }}
+
+        
+        <footer class="iq-footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
+                            <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 text-lg-end text-center mt-lg-0 mt-3">
+                        <span class="mr-1">
+                            © Copyright&nbsp;
+                            <script>document.write(new Date().getFullYear())</script>&nbsp;<a href="#" class="">Datum</a> |
+                            All Rights Reserved.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </footer>    <!-- Library Bundle Script -->
+        <script src="{{ asset('assets/js/core/libs.min.js')}}"></script>
+        <script src="{{ asset('assets/js/core/external.min.js')}}"></script>
+        <!-- Chart Custom JavaScript -->
+        <script src="{{ asset('assets/js/customizer.js')}}"></script>
+        
+        <script src="{{ asset('assets/js/sidebar.js')}}"></script>
+        
+        <!-- Flextree Javascript-->
+        <script src="{{ asset('assets/js/flex-tree.min.js')}}"></script>
+        <script src="{{ asset('assets/js/tree.js')}}"></script>
+        
+        <!-- Table Treeview JavaScript -->
+        <script src="{{ asset('assets/js/table-treeview.js')}}"></script>
+        
+        <!-- SweetAlert JavaScript -->
+        <script src="{{ asset('assets/js/sweetalert.js')}}"></script>
+        
+        <!-- Vectoe Map JavaScript -->
+        <script src="{{ asset('assets/js/vector-map-custom.js')}}"></script>
+        
+        <!-- Chart Custom JavaScript -->
+        <script src="{{ asset('assets/js/chart-custom.js')}}"></script>
+        <script src="{{ asset('assets/js/charts/01.js')}}"></script>
+        
+        <!-- slider JavaScript -->
+        <script src="{{ asset('assets/js/slider.js')}}"></script>
+        
+        <!-- Emoji picker -->
+        <script src="{{asset('assets/vendor/emoji-picker-element/index.js')}}" type="module"></script>
+        
+        
+        <!-- app JavaScript -->
+        <script src="{{ asset('assets/js/app.js')}}"></script>
+        
+      </body>
+    </html>
