449f7a3ac86dfb09eac4bf2f45874edcaef0dd80
aditi-19112002 <aditikeshri21@gmail.com>
2025-03-25 07:51:49 +0530

    contact responsive work done

--- resources/views/livewire/public/contact.blade.php
+++ resources/views/livewire/public/contact.blade.php
@@ -50,15 +50,34 @@
                     </div>
 
                   
-                    <div class="space-y-8">
+                     <div class="space-y-8">
+            
                         <div class="bg-gray-100 p-6 rounded-lg shadow-lg">
                             <h3 class="text-2xl font-semibold mb-6">Contact Information</h3>
                             <div class="space-y-4">
-                                <p class="flex items-center justify-center md:justify-start"><svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> 123 Comestro, Purnea, Bihar</p>
-                                <p class="flex items-center justify-center md:justify-start"><svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg> +1 234 567 890</p>
-                                <p class="flex items-center justify-center md:justify-start"><svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg> comessto@gmail.com</p>
+                                <p class="flex items-center justify-start md:justify-start">
+                                    <svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                    </svg> 
+                                    123 Comestro, Purnea, Bihar
+                                </p>
+                                <p class="flex items-center justify-start md:justify-start">
+                                    <svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
+                                    </svg> 
+                                    <a href="tel:+919546805580" class="text-black hover:underline">+919546805580</a>
+                                </p>
+                                <p class="flex items-center justify-start md:justify-start">
+                                    <svg class="w-5 h-5 mr-2 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                    </svg> 
+                                    <a href="mailto:comestrotechlabs@gmail.com" class="text-black hover:underline">comestrotechlabs@gmail.com</a>
+                                </p>
                             </div>
                         </div>
+                        
+                        
 
                         <div class="bg-gray-100 p-6 rounded-lg shadow-lg">
                             <h3 class="text-2xl font-semibold mb-6">Our Location</h3>
@@ -69,7 +88,8 @@
                                 </iframe>
                             </div>
                         </div>
-                    </div>
+                    </div> 
+                    
                 </div>
             </div>
         </div>
