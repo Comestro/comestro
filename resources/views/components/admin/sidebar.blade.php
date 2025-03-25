2dde13518f73d503e207b64c877d6434399474a0
aditi-19112002 <aditikeshri21@gmail.com>
2025-03-25 12:46:39 +0530

    some change

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -429,7 +429,7 @@
                         </ul>
                     </li>
 
-                    <li class="sidebar-layout">
+                    {{-- <li class="sidebar-layout">
                         <a href="#app9" class="collapsed svg-icon" data-bs-toggle="collapse" aria-expanded="false">
                             <i>
                                
@@ -455,11 +455,11 @@
                                 </a>
                             </li>
                         </ul>
-                    </li>
+                    </li> --}}
                     <li class="sidebar-layout">
                         <a href="#works" class="collapsed svg-icon" data-bs-toggle="collapse" aria-expanded="false">
                             <i>
-                                <!-- SVG for Works (e.g., a "briefcase" icon symbolizing projects/work) -->
+                               
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
