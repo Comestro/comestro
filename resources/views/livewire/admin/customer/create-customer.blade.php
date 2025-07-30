08677705b9878c547ca421981ef42379702aa742
aditi-19112002 <aditikeshri21@gmail.com>
2025-07-30 23:57:35 +0530

    refactor: restructure New Customer section for improved layout

--- resources/views/livewire/admin/customer/create-customer.blade.php
+++ resources/views/livewire/admin/customer/create-customer.blade.php
@@ -15,7 +15,10 @@
             </div>
 
             <div class="col-lg-12 mb-3">
-                <h4 class="fw-bold">New Customer</h4>
+                <div class="d-flex align-items-center justify-content-between">
+                    <h4 class="fw-bold">New Customer</h4>
+                    <a href="{{ route('admin.customers.manage-customer') }}" class="btn btn-primary">Go to Manage</a>
+                </div>
             </div>
 
         
